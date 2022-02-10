ec9ce2899327050deb65aeb71a6445d741eeea0a
KayStation <matt0122@comcast.net>
2022-02-10 01:55:18 -0600

    Proper horizontal line breaks

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,23 @@
-const container = document.getElementById('container');
 
 
 const createRow = function(rowLength){
+    const container = document.getElementById('container');
+    const hr = document.createElement('hr');
+
     for(let i = 0; i < rowLength; i++){
         const div = document.createElement('div');
         div.classList.add('gridBlock');
         div.textContent = '\u00a0';
         container.appendChild(div);
-    }
+    };
+    
+    container.appendChild(hr);
 }
 
 const createGrid = function(size){
-    createRow(size);
-}
+    for(let i = 0; i < size; i++){
+        createRow(size);
+    };
+};
+
+createGrid(15);
